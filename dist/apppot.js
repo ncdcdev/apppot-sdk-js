25d3f09ddb68965dfee5c72d770506c0fec4474d
karuru6225 <e2hjhi@gmail.com>
2017-06-07 22:52:11 +0900

    ビルド時にバージョン更新を忘れた。

--- dist/apppot.js
+++ dist/apppot.js
@@ -135,10 +135,10 @@ return /******/ (function(modules) { // webpackBootstrap
 	        return this._localDb;
 	    };
 	    AppPot.prototype.getBuildDate = function () {
-	        return (1496828967) || "unknown";
+	        return (1496843513) || "unknown";
 	    };
 	    AppPot.prototype.getVersion = function () {
-	        return (["2","3","23"]).join('.') || "unknown";
+	        return (["2","3","25"]).join('.') || "unknown";
 	    };
 	    AppPot.prototype.log = function (str, level) {
 	        var _this = this;
